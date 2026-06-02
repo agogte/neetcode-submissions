6498cd0607f02f078403cb8e469f8beafca39639
Advait Gogte <90155284+agogte@users.noreply.github.com>
2026-06-02 12:11:08 -0400

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.js
@@ -0,0 +1,55 @@
+class Twitter {
+    constructor() {
+        this.postMap = new Map(); //{key: userId, val: Set(tweetIds)}
+        this.followMap = new Map(); //{key: userId, val: Set(followings' userid)};
+        this.timeStamp = 0;
+    }
+
+    /**
+     * @param {number} userId
+     * @param {number} tweetId
+     * @return {void}
+     */
+    postTweet(userId, tweetId) {
+        let newTweet = { id: tweetId, time: this.timeStamp++ };
+        this.postMap.set(userId, (this.postMap.get(userId) ?? []).concat(newTweet));
+    }
+
+    /**
+     * @param {number} userId
+     * @return {number[]}
+     */
+    getNewsFeed(userId) {
+        let followees = this.followMap.get(userId) ?? new Set([userId]);
+        let minHeap = new MinPriorityQueue((tweet) => tweet.time);
+        for(let followee of [...followees]){
+            let followeeTweets = this.postMap.get(followee) ?? [];
+            for(let tweet of followeeTweets){
+               minHeap.enqueue(tweet);
+
+               if(minHeap.size() > 10)
+                    minHeap.dequeue();
+            }
+        }
+        return [...minHeap].sort((a,b) => b.time - a.time).map(tweet => tweet.id);
+    }
+
+    /**
+     * @param {number} followerId
+     * @param {number} followeeId
+     * @return {void}
+     */
+    follow(followerId, followeeId) {
+        this.followMap.set(followerId, (this.followMap.get(followerId) ?? new Set([followerId])).add(followeeId));
+    }
+
+    /**
+     * @param {number} followerId
+     * @param {number} followeeId
+     * @return {void}
+     */
+    unfollow(followerId, followeeId) {
+        if(followerId !== followeeId)
+            this.followMap.get(followerId).delete(followeeId);
+    }
+}
