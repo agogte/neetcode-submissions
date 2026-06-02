8e501839335566f4754a94caf48d063b2fc9efe5
Advait Gogte <90155284+agogte@users.noreply.github.com>
2026-06-02 13:18:42 -0400

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.js
@@ -0,0 +1,39 @@
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.cache = new Map();
+        this.capacity = capacity;
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        if(!this.cache.has(key)) return -1;
+        let value  = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, value);
+        return value;
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        // if(!this.cache.has(key)){
+        //     this.cache.set(key, value);
+        // }
+        if(this.cache.has(key))
+            this.cache.delete(key)
+        else if(this.capacity === this.cache.size){
+            let expiredCacheKey = [...this.cache.keys()][0];
+            this.cache.delete(expiredCacheKey)
+        }
+        this.cache.set(key, value);
+    }
+}
