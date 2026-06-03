007c3131629c0504a1d85d4b64193f5aa45a2d78
Advait Gogte <90155284+agogte@users.noreply.github.com>
2026-06-02 21:54:17 -0400

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        intervals.sort((a,b) => a[0] - b[0]);
+        let output = [intervals[0]];
+        for(let [start, end] of intervals){
+            let lastIntervalEnd = output[output.length - 1][1];
+            if(lastIntervalEnd >= start)
+                output[output.length - 1][1] = Math.max(end, lastIntervalEnd);
+            else
+                output.push([start, end]);
+        }
+        return output;
+    }
+}
