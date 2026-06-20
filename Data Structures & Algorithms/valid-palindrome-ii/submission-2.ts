c1f5c521c5cb5e8b3102d18052f78d22795254a8
Advait Gogte <90155284+agogte@users.noreply.github.com>
2026-06-20 13:41:03 -0400

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s: string): boolean {
+        let left: number = 0;
+        let right: number = s.length - 1;
+        while(left < right){
+            if(s[left] === s[right]){
+                left++;
+                right--;
+            }
+            else {
+                return (this.isPalindrome(s, left+1, right)
+                || this.isPalindrome(s, left, right - 1))
+            }
+        }
+
+
+        return true;
+    }
+
+    isPalindrome(s: string, left: number, right: number) {
+        // let left = 0; let right = s.length - 1
+        while(left < right){
+            if(s[left] !== s[right])
+                return false;
+
+            right--;
+            left++;
+        }
+        return true;
+    }
+}
